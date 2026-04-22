269e90ecdb90bec7301e75bbe30ac8c4cac462b7
savio11904 <svdsv288@gmail.com>
2026-04-22 15:20:05 +0100

    Remove database creation commands from SQL file
    Removed database creation and use statements.

--- base de dados/create.sql
+++ base de dados/create.sql
@@ -1,7 +1,3 @@
--- Base de Dados FilmFlow
-CREATE DATABASE filmflow_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE filmflow_db;
-
 -- Tabela de Localizações
 CREATE TABLE localizacoes (
     id      INT AUTO_INCREMENT PRIMARY KEY,
